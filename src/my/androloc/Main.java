e23ea2ce28dec31f836eb8d44d46f6f52f44c320
sneek0r <sneek0r@freenet.de>
2010-05-31 23:36:25 +0200

    locationlistener implemented

--- src/my/androloc/Main.java
+++ src/my/androloc/Main.java
@@ -1,13 +1,105 @@
 package my.androloc;
 
 import android.app.Activity;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.text.format.Time;
+import android.widget.TextView;
 
-public class Main extends Activity {
+public class Main extends Activity implements LocationListener {
+	
+	public LocationManager manager;
+		
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    @Override
+    public void onStart() {
+    	// get system location manager
+    	this.manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    	
+    	Criteria criteria = new Criteria();
+    	criteria.setAccuracy(Criteria.ACCURACY_FINE);
+    	criteria.setPowerRequirement(Criteria.POWER_MEDIUM);
+    	// Indicates whether the provider is allowed to incur monetary cost. 
+    	criteria.setCostAllowed(false);
+    	criteria.setAltitudeRequired(true);
+    	criteria.setBearingRequired(true);
+    	criteria.setSpeedRequired(true);
+    	
+    	// get provider described by criteria
+    	String provider = this.manager.getBestProvider(criteria, true);
+    	
+    	// request location updates
+    	this.manager.requestLocationUpdates(provider, 60000, 10, this);
+    }
+    
+    @Override
+    public void onStop() {
+    	// remove location updates
+    	this.manager.removeUpdates(this);
+    }
+
+	public void onLocationChanged(Location location) {
+		float accuracy = location.getAccuracy();
+		double altitude = location.getAltitude();
+		// Returns the direction of travel in degrees East of true North
+		float bearing = location.getBearing();
+		double latitude = location.getLatitude();
+		double longtitude = location.getLongitude();
+		float speed = location.getSpeed();
+		long time = location.getTime();
+		
+		TextView accuracy_tv = (TextView)findViewById(R.id.accuracy);
+		accuracy_tv.setText(Float.toString(accuracy)+"m");
+		accuracy_tv.postInvalidate();
+		
+		TextView altitude_tv = (TextView)findViewById(R.id.altitude);
+		altitude_tv.setText(Double.toString(altitude));
+		altitude_tv.postInvalidate();
+		
+		TextView bearing_tv = (TextView)findViewById(R.id.bearing);
+		bearing_tv.setText(Float.toString(bearing));
+		bearing_tv.postInvalidate();
+
+		TextView latitude_tv = (TextView)findViewById(R.id.latitude);
+		latitude_tv.setText(Location.convert(latitude, Location.FORMAT_MINUTES));
+		latitude_tv.postInvalidate();
+
+		TextView longtitude_tv = (TextView)findViewById(R.id.longtitude);
+		longtitude_tv.setText(Location.convert(longtitude, Location.FORMAT_MINUTES));
+		longtitude_tv.postInvalidate();
+
+		TextView speed_tv = (TextView)findViewById(R.id.speed);
+		speed_tv.setText(Float.toString(speed));
+		speed_tv.postInvalidate();
+		
+		TextView time_tv = (TextView)findViewById(R.id.time);
+		Time t = new Time(); t.set(time);
+		time_tv.setText(DateFormat.format("kk:mm.ss", time));
+		time_tv.postInvalidate();
+	}
+
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
 }
